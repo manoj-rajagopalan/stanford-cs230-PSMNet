a195a146a87b6aeca534688149ca3c0c582f78ab
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2020-11-08 02:05:50 +0000

    Fix myrun.sh - no spaces after \

--- myrun.sh
+++ myrun.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 PYTHONPATH=${PYTHONPATH}:models \
-python main.py --maxdisp 192 \
-               --model stackhourglass \
-               --datapath dataset/ \
-               --epochs 10 \
-               --savemodel ./output
+python main.py \
+    --tag 01-train_bs_8-crop_w_256-no_pretrain \
+    --comment "Reduced training batch size to 8 (was 12). Cropped image width to 256 (Was 512). No pre-trained model." \
+    --maxdisp 192 \
+    --model stackhourglass \
+    --datapath ./dataset/ \
+    --epochs 10 \
+    --savemodel ./output/
 
 
 
